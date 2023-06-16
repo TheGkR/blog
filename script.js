c6caa7b0eea726297744472244b495e3fe986e7f
Goutham <131663891+TheGkR@users.noreply.github.com>
2023-06-16 14:04:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ var lastScrollTop = 0;
 window.addEventListener('scroll', function() {
     var firstPart = document.querySelector('.first-part');
     var secondPart = document.querySelector('.second-part');
+    var button = document.querySelector('.scroll-button');
 
     var st = window.pageYOffset || document.documentElement.scrollTop;
 
     if (st > lastScrollTop) {
-        // Scrolling downwards, hide contents
-        firstPart.classList.add('hide-contents');
+        // Scrolling downwards
+        if (!secondPart.classList.contains('active')) {
+            firstPart.classList.add('hide-contents');
+        }
     } else {
-        // Scrolling upwards, show contents
-        firstPart.classList.remove('hide-contents');
+        // Scrolling upwards
+        if (!secondPart.classList.contains('active')) {
+            firstPart.classList.remove('hide-contents');
+        }
     }
 
     lastScrollTop = st <= 0 ? 0 : st;
